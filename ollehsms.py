1160451d7bb87a782983e59e12b3b07a35c5d82b
Chang-young Koh <gkcy1019@gmail.com>
2016-08-03 20:41:18 +0900

    Add Compatibility

--- ollehsms.py
+++ ollehsms.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import re
-import urllib
-import requests as rq
-# Since requests doesn't have -*- coding -*-.
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import urllib
+import codecs
+import requests as rq
 
 
 """
 # OllehSMS
 
-Python API wrapper for Olleh Web SMS Sending Service.
+Python API wrapper for KT(Olleh) Web SMS Sending Service.
 
 ## USAGE
 
     msg = r'Let's send SMS programatically!'
     sms = OllehSMS()
-    print 'auth', sms.auth('my_id', 'some_fancy_strong_password')  # auth True
-    print 'send', sms.send(msg, ['010-XXXX-XXXX'])  # send True
+    print 'auth', sms.auth('my_id', 'some_fancy_strong_password')
+    print 'send', sms.send(u'my_UNICODE_msg', ['010-XXXX-XXXX'])
 
 """
+
+if sys.stdout.encoding != 'UTF-8':
+    print('Please set PYTHONENCODING=UTF-8 in your environment then retry.', file=sys.stdout)
 
 
 class OllehSMS:
@@ -32,8 +34,10 @@ class OllehSMS:
 
     PATTERN_REMAINING_FREE = re.compile(r'freeSmsCnt" value="([0-9]+)"', re.IGNORECASE)
     PATTERN_MY_PHONE = re.compile(r'var _my_phone = "([^"]+)"', re.IGNORECASE)
-    DATA_SEND_CONFIRM = r'dispmsg=S&dispmsgType=smsPortal&sendcnt=1&ccnt=0&cmsg=&isReserv=N&cid=0&stamp_cnt=0&evt_cnt=0&ISSMSSTAT=SMS&all_cnt=1'
-    __VERSION__ = '0.1'
+
+    SMS_MAXIMUM_LENGTH = 73
+
+    __version__ = '0.1'
 
     def __init__(self):
         self.sess = rq.Session()
@@ -41,10 +45,19 @@ class OllehSMS:
         self.free = 0
 
     def auth(self, id, pw):
+        """Login and prepare for sending SMS.
+        Args:
+            id (str): olleh.com ID.
+            pw (str): olleh.com password.
+
+        Returns:
+            bool: True if successful.
+        """
         auth_payload = {'userId': id,
                         'password': pw,
                         'checkSavedId': 'Y'}
-        r = self.sess.post(OllehSMS.URL_AUTH, data=auth_payload)
+        self.sess.get(OllehSMS.URL_INIT)
+        self.sess.post(OllehSMS.URL_AUTH, data=auth_payload)
         response = self.sess.get(OllehSMS.URL_REMAINING_FREE).text
         self.free = OllehSMS.PATTERN_REMAINING_FREE.search(response)
         if not self.free:
@@ -53,23 +66,26 @@ class OllehSMS:
         return True
 
     def send(self, msg, recipients):
-        if not isinstance(msg, unicode):
-            msg = msg.decode('utf-8')
+        if hasattr(str, 'decode') and not isinstance(msg, unicode):
+            msg = msg.decode('utf-8')  # Python 2
         msg = msg.encode('euc-kr')
         size = len(msg)
         target = ','.join((x.replace('-', '') for x in recipients)) + ','
 
         msg_payload = {'freeSmsCnt': self.free,
-                        'recvCnt': len(recipients),
-                        'recvLists': target,
-                        'recvNames': target,
-                        'msgType': 'SMS',
-                        'isGroups': 'N',
-                        'authChk': 'OK',
-                        'smsSndChk': 'N',
-                        'editInput': msg,
-                        'dataLen': size,
-                        'mmsArea': '<body></body>'}
+                       'recvCnt': len(recipients),
+                       'recvLists': target,
+                       'recvNames': target,
+                       'msgType': 'SMS',
+                       'isGroups': 'N',
+                       'authChk': 'OK',
+                       'smsSndChk': 'N',
+                       'editInput': msg,
+                       'dataLen': size,
+                       'mmsArea': '<body></body>'}
+
+        if size > 73:
+            msg_payload['msgType'] = 'LTS'
 
         r = self.sess.post(OllehSMS.URL_SEND_ENQUEUE, data=msg_payload).text
         r = OllehSMS.PATTERN_MY_PHONE.search(r)
@@ -79,21 +95,33 @@ class OllehSMS:
             my_phone = r.group(1)
 
         msg_payload['MsgDetailType'] = 'A00'
-        msg_payload['sendhtml'] = msg_payload['editInput']
-        msg_payload['recvLists'] = msg_payload['recvLists'].encode('base64')[:-1]
-        msg_payload['recvNames'] = msg_payload['recvNames'].encode('base64')[:-1]
-        msg_payload['sndIdx'] = msg_payload['isDeco'] = msg_payload['isDecoBg'] = ''
-        msg_payload['picIdx'] = msg_payload['PhotoUrl'] = msg_payload['starSMS'] = ''
-        msg_payload['reserved'] = msg_payload['autoName'] = msg_payload['cIdx'] = ''
-        msg_payload['recvConfirm'] = msg_payload['recvPhone'] = msg_payload['readConfirm'] = ''
+        confirm_url = OllehSMS.URL_SEND_CONFIRM
+        self.free -= 1
 
-        msg_payload_serialized = urllib.urlencode(msg_payload)
+        if size > 73:  # In case of LMS
+            msg_payload['MsgDetailType'] = 'D00'
+            confirm_url = OllehSMS.URL_SEND_CONFIRM.replace('smsSend', 'mmsSend')
+            self.free -= 1  # LMS consumes additional Free SMS count.
+
+        msg_payload['sendhtml'] = msg_payload['editInput']
+
+        if hasattr(urllib, 'urlencode'):  # Python 2
+            msg_payload['recvLists'] = msg_payload['recvLists'].encode('base64')[:-1]
+            msg_payload['recvNames'] = msg_payload['recvNames'].encode('base64')[:-1]
+            msg_payload_serialized = urllib.urlencode(msg_payload)
+        else:  # Python 3
+            msg_payload['recvLists'] = codecs.encode(bytes(msg_payload['recvLists'], 'ascii'), 'base64')
+            msg_payload['recvNames'] = codecs.encode(bytes(msg_payload['recvNames'], 'ascii'), 'base64')
+            msg_payload_serialized = urllib.parse.urlencode(msg_payload)
+
         msg_payload_serialized += '&sendPhone=' + my_phone
 
-        self.sess.post(OllehSMS.URL_SEND_CONFIRM, data=msg_payload_serialized,
-                headers={"Content-Type":
-                         "application/x-www-form-urlencoded  charset=euc-kr",
-                         "X-Project": "pcMessenger",
-                         "X-Version": "1.0.1",
-                         "X-Request-With": "XMLHttpRequest"})
+        self.sess.post(confirm_url, data=msg_payload_serialized,
+                       headers={"Content-Type":
+                                "application/x-www-form-urlencoded  charset=euc-kr",
+                                "X-Project": "pcMessenger",
+                                "X-Version": "1.0.1",
+                                "X-Request-With": "XMLHttpRequest"})
+
+        # TODO: Check result.
         return True
